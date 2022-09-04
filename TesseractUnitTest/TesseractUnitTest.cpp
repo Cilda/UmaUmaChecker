14559f0a59c22ee2c58620682eb58421ee29ae0d
Cilda <mile@hotmail.co.jp>
2022-09-04 23:27:25 +0900

    [add] UnitTest追加
    Tesseract確認用UnitTestを追加

---
+++ TesseractUnitTest/TesseractUnitTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TesseractUnitTest
+{
+	TEST_CLASS(TesseractUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
