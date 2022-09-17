6d17460d23e2ba75ab8490fee51ba6d770b67472
Cilda <mile@hotmail.co.jp>
2022-09-17 16:06:57 +0900

    [add] 置換クラス追加
    イベント名検出の妨げになる文字は置換するためので、クラス追加

---
+++ UmaUmaChecker/TextCollector.cpp
@@ -0,0 +1,52 @@
+#include <string>
+
+#include "TextCollector.h"
+
+#include <sstream>
+#include <fstream>
+#include <nlohmann/json.hpp>
+#include <regex>
+
+#include "utility.h"
+
+using json = nlohmann::json;
+
+TextCollector::TextCollector()
+{
+}
+
+TextCollector::~TextCollector()
+{
+}
+
+void TextCollector::Load()
+{
+	std::fstream stream(utility::GetExeDirectory() + L"\\Library\\ReplaceText.json");
+	if (stream.good()) {
+		std::stringstream text;
+
+		text << stream.rdbuf();
+
+		try {
+			json replaces = json::parse(text.str());
+
+			for (auto& arr : replaces["replaces"]) {
+				collections.emplace_back(
+					utility::ConvertUtf8ToUtf16(arr["search"].get<std::string>().c_str()),
+					utility::ConvertUtf8ToUtf16(arr["replace"].get<std::string>().c_str())
+				);
+			}
+		}
+		catch (json::exception& ex) {
+		}
+	}
+}
+
+void TextCollector::Collect(std::wstring& text)
+{
+	for (auto& c : collections) {
+		std::wregex reg(c.Search);
+
+		text = std::regex_replace(text, reg, c.Replace);
+	}
+}
