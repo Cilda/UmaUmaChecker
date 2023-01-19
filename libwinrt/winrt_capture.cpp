8b6300bfa18599aa3232c2814d868713c9144dbe
Cilda <mile@hotmail.co.jp>
2023-01-19 19:28:16 +0900

    [fix] DLLプロジェクトにEXPORT追加
    EXPORTを定義、ソースにEXPORT追加

--- libwinrt/winrt_capture.cpp
+++ libwinrt/winrt_capture.cpp
@@ -2,28 +2,30 @@
 
 #include "winrt_capture.h"
 
-void __stdcall winrt_init_apartment()
+EXPORT void __stdcall winrt_init_apartment()
 {
 	winrt::init_apartment(winrt::apartment_type::single_threaded);
 }
 
-struct GraphicsCapture* __stdcall winrt_init_capture(HWND hWnd)
+EXPORT struct GraphicsCapture* __stdcall winrt_init_capture(HWND hWnd)
 {
 	GraphicsCapture* capture = new GraphicsCapture(hWnd);
 	return capture;
 }
 
-void __stdcall free_winrt_capture(GraphicsCapture* capture)
+EXPORT void __stdcall free_winrt_capture(GraphicsCapture* capture)
 {
 	delete capture;
 }
-Gdiplus::Bitmap* __stdcall winrt_screenshot(struct GraphicsCapture* capture)
+
+EXPORT Gdiplus::Bitmap* __stdcall winrt_screenshot(struct GraphicsCapture* capture)
 {
 	if (!capture) return nullptr;
 
 	return capture->ScreenShot();
 }
-HWND __stdcall winrt_capture_get_target(struct GraphicsCapture* capture)
+
+EXPORT HWND __stdcall winrt_capture_get_target(struct GraphicsCapture* capture)
 {
 	if (!capture) return NULL;
 
