23dc66c01772466ef0bf57258514da70c9f98be9
Cilda <mile@hotmail.co.jp>
2023-04-21 01:03:49 +0900

    [update] バージョンアップ
    v1.4.5にバージョンアップ

--- UmaUmaChecker/version.h
+++ UmaUmaChecker/version.h
@@ -9,6 +9,6 @@
 #endif
 
 static const wxString app_name = wxT("ウマウマチェッカー");
-static const wxString app_version = wxT("v1.4.4");
+static const wxString app_version = wxT("v1.4.5");
 static const wxString app_copyright = wxT("Copyright © 2022-2023 Cilda.");
 static const wxString app_title = app_name + wxT(" ") + app_version + DEBUG_STRING;
