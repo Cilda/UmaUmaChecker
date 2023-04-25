fa3e9afbb06f403982d1cec35bc8679616312c3c
Cilda <mile@hotmail.co.jp>
2023-04-26 01:27:12 +0900

    [fix] #60 修正
    選択肢名から識別した際に共通イベントは選択キャラクターのイベントとなるよう、イベント名からCurrentCharacterのイベントを取得する

--- UmaUmaChecker/EventData.cpp
+++ UmaUmaChecker/EventData.cpp
@@ -128,7 +128,7 @@ std::shared_ptr<EventSource> EventData::RetrieveTitle(const std::wstring& title,
 	}
 }
 
-std::shared_ptr<EventSource> EventData::RetrieveOption(const std::wstring& option)
+std::shared_ptr<EventSource> EventData::RetrieveOption(const std::wstring& option, EventRoot* root)
 {
 	std::vector<std::wstring> xstrs;
 
@@ -147,6 +147,10 @@ std::shared_ptr<EventSource> EventData::RetrieveOption(const std::wstring& optio
 
 	const auto& event = OptionMap.find(match);
 	if (event == OptionMap.end()) return nullptr;
+	else if (root) {
+		const auto exactEvent = root->Events.find(event->second->Name);
+		if (exactEvent != root->Events.end()) return exactEvent->second;
+	}
 
 	return event->second;
 }
